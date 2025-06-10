ed5f52f444b921c27218c075b9f8770c7c0e4c6f
Srishti Sinha <srishtiisinha2003@gmail.com>
2025-06-10 21:00:23 +0530

    added file

---
+++ Microsoft/Microsoft(medium)/IVSum.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class IVSum {
+    List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length < 4) return res;
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int left = j + 1, right = n - 1;
+                while (left < right) {
+                        long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+                        if (sum == target) {
+                            res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                            while (left < right && nums[left] == nums[left + 1]) left++;
+                            while (left < right && nums[right] == nums[right - 1]) right--;
+                            left++;
+                            right--;
+                        } else if (sum < target) {
+                            left++;
+                        } else {
+                            right--;
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+    public static void main(String[] args) {
+        IVSum ivSum = new IVSum();
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        int target = 0;
+        List<List<Integer>> result = ivSum.fourSum(nums, target);
+        System.out.println(result); // Expected output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+    }
+}
+
